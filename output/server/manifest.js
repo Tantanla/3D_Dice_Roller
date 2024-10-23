ac462f1bd312c9dd2cde7935379bfe7ded1b57bc
tantanla <3567952572@qq.com>
2024-10-23 15:24:05 +0800

    Updates

--- output/server/manifest.js
+++ output/server/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["favicon.png","robots.txt"]),
 	mimeTypes: {".png":"image/png",".txt":"text/plain"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.JvWa5rhm.js","app":"_app/immutable/entry/app.CtsLLi0q.js","imports":["_app/immutable/entry/start.JvWa5rhm.js","_app/immutable/chunks/entry.Cavd7pCU.js","_app/immutable/chunks/scheduler.BvLojk_z.js","_app/immutable/entry/app.CtsLLi0q.js","_app/immutable/chunks/scheduler.BvLojk_z.js","_app/immutable/chunks/index.BufXLbuI.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
+		client: {"start":"_app/immutable/entry/start.BJqTZFwe.js","app":"_app/immutable/entry/app.BQIGwsmQ.js","imports":["_app/immutable/entry/start.BJqTZFwe.js","_app/immutable/chunks/entry.NkmKj5l0.js","_app/immutable/chunks/scheduler.BvLojk_z.js","_app/immutable/entry/app.BQIGwsmQ.js","_app/immutable/chunks/scheduler.BvLojk_z.js","_app/immutable/chunks/index.BufXLbuI.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js'))
